dd8847f4a58d1e7558ef6e8b11aad0cbb396cd88
Adriana Capriles <adrianacapriles@gmail.com>
2018-05-20 21:04:20 -0400

    working psychic game with style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+// VARIABLES
+
 var alphabet = "abcdefghijklmnopqrstuvwxyz".toLowerCase();
 
 var userArray = [];
@@ -11,6 +13,7 @@ var computerGuess = computerGuess;
 computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)]; {
     console.log(computerGuess);
 }
+
 function computerChoices(){
     computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)]; {
         console.log(computerGuess); 
@@ -25,6 +28,7 @@ document.onkeyup = function(event) {
     // Determines which key was pressed.
     var userGuess = event.key;
 
+    // Array for user guesses
     if (userArray.indexOf(userGuess) >=0) {
     }
     else {
@@ -33,24 +37,27 @@ document.onkeyup = function(event) {
         console.log(userArray);
     }
 
+    // if computer guess get a point
     if (userGuess === computerGuess) {
       wins++;
       guessLeft = 9;
       userArray = [];
 
     }
-
+    // if user wrong, user loss 1 guess 
     computerChoices()
     if (userGuess !== computerGuess) {
         guessLeft --;
     }
 
+    //if users guess are 0, user losses
     if (guessLeft==0) {
         losses++;
         guessLeft = 9;
         userArray = [];
     }
 
+    // CALLS (Execution)
     document.getElementById("wins").innerHTML = wins;
     document.getElementById("losses").innerHTML = losses;
     document.getElementById("guessLeft").innerHTML = guessLeft;
